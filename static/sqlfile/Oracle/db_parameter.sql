dc43e5595ba0daaa1be012d4c8de8ad4cea419e8
chongzi <15201061106@163.com>
2021-11-08 18:27:11 +0800

    modify

--- static/sqlfile/Oracle/db_parameter.sql
+++ static/sqlfile/Oracle/db_parameter.sql
@@ -1,28 +1,23 @@
 select
-'db_check_db_parameter' as "db_check_db_parameter",
-ksppinm as name,
-ksppstvl value,
-case when ksppinm = '_optim_peek_user_binds' and ksppstvl <> 'TRUE'
-      then 'Recommend set to default value <TRUE>, Peep through statistical information histogram
- binding variables'
+    'db_check_db_parameter' as "db_check_db_parameter",
+    ksppinm as name,
+    ksppstvl as value,
+    case when ksppinm = '_optim_peek_user_binds' and ksppstvl <> 'TRUE'
+      then 'Recommend set to default value <TRUE>, Peep through statistical information histogram binding variables'
      when ksppinm = '_undo_autotune' and ksppstvl = 'TRUE'
       then 'Recommend set to <FALSE>, default value is TRUE Doc 1574714.1'
      when ksppinm = '_optimizer_adaptive_cursor_sharing' and ksppstvl <> 'TRUE'
-      then 'Recommend set to <TRUE> to solves the execution plan remains unchanged after binding
-variable peeping issue'
+      then 'Recommend set to <TRUE> to solves the execution plan remains unchanged after binding variable peeping issue'
      when ksppinm = '_optimzer_adaptive_extended_cursor_sharing' and ksppstvl <> 'UDO'
-      then 'should be set to <UDO> to solves the problem that the execution plan remains unchange
-d after binding variable peeping issue'
+      then 'should be set to <UDO> to solves the problem that the execution plan remains unchanged after binding variable peeping issue'
      when ksppinm = '_optimizer_extended_cursor_sharing_rel' and  ksppstvl <> 'SIMPLE'
-      then 'Should be set to <SIMPLE> to solves the problem that the execution plan remains uncha
-nged after binding variable peeping issue'
+      then 'Should be set to <SIMPLE> to solves the problem that the execution plan remains unchanged after binding variable peeping issue'
      when ksppinm = 'optimizer_index_caching' and ksppstvl <> 0
       then 'optimizer_index_caching should set to <0>, default value is 0'
      when ksppinm = 'optimizer_index_cost_adj' and ksppstvl <> 100
       then 'optimizer_index_cost_adj should set to <100>, default value is 100'
      when ksppinm = '_serial_direct_read' and ksppstvl <> 'auto'
-      then 'Recommend to set to <auto>, Only when the table is small can we consider setting it t
-o never'
+      then 'Recommend to set to <auto>, Only when the table is small can we consider setting it to never'
      when ksppinm = '_gc_policy_time' and ksppstvl <> 0
       then '_gc_policy_time recommend to set to <0> to global close DRM'
      when ksppinm = '_gc_undo_affinity' and ksppstvl = 'TRUE'
@@ -46,123 +41,118 @@ o never'
      when ksppinm = '_use_single_log_writer' and ksppstvl <> 'TRUE'
       then '12c new features recommed to set to <TRUE>'
      when ksppinm = '_use_adaptive_log_file_sync' and ksppstvl ='TRUE'
-      then '11203 and above Version default <TRUE> May Cause log sync Issue,refer to DOC 1462942.
-1'
+      then '11203 and above Version default <TRUE> May Cause log sync Issue,refer to DOC 1462942.1'
      when ksppinm = '_datafile_write_errors_crash_instance' and ksppstvl = 'TRUE'
       then 'Recommed to set to False, Instance Crashes After IO Error Doc 2453717.1'
      when ksppinm = '_clusterwide_global_transactions' and ksppstvl <> 'TRUE'
-      then 'In RAC Env,May Cause 2PC Transaction Issue'
+      then 'In RAC Env,May Cause 2PC Transaction Issue, Set <True>'
      when ksppinm = '_kcfis_storageidx_diag_mode' and ksppstvl = '2'
       then 'Open the Debug mode for storage index on the cell'
-end as advice
---,ksppdesc
-from x$ksppi x, x$ksppcv y
-where x.indx = y.indx
-and ksppinm in
-('_kcfis_storageidx_diag_mode',
-'__db_cache_size',
-'_bloom_filter_enabled',
-'_memory_imm_mode_without_autosga',
-'_small_table_threshold',
-'_very_large_object_threshold',
-'_kcfis_storageidx_disabled',
-'_serial_direct_read',
-'_very_large_object_threshold',
-'_max_outstanding_log_writes',
-'_use_single_log_writer',
-'_datafile_write_errors_crash_instance',
-'_use_adaptive_log_file_sync',
-'_undo_autotune',
-'_optim_peek_user_binds',
-'_optimizer_use_feedback',
-'_optimizer_adaptive_cursor_sharing',
-'_optimizer_extended_cursor_sharing',
-'_optimizer_extended_cursor_sharing_rel',
-'optimizer_index_caching',
-'optimizer_index_cost_adj',
-'db_file_multiblock_read_count',
-'optimizer_dynamic_sampling',
-'_cleanup_rollback_entries',
-'_cursor_obsolete_threshold',
-'_b_tree_bitmap_plans',
-'optimizer_mode',
-'_gc_policy_time',
-'_gc_undo_affinity',
-'optimizer_adaptive_plans',
-'_PX_use_large_pool',
-'parallel_force_local',
-'_cursor_obsolete_threshold',
-'_clusterwide_global_transactions',
-'optimizer_adaptive_features',
-'_optimizer_inmemory_access_path',
-'_sql_plan_directive_mgmt_control',
-'_optimizer_dsdir_usage_control'
-)
-union all
-select
-'db_check_db_parameter' as "db_check_db_parameter",
-name,
-value,
-case when name = 'open_cursors' and (value < 300 or value > 1000) then 'open_cursors recommend to
- set to more than <300>, 1000 is enough'
-     when name = 'control_file_record_keep_time' and value <= 7 then 'control_file_record_keep_ti
-me recommend to set to <15>, 30 is enough,Doc 47322.1'
-     when name = 'cursor_sharing' and value <> 'EXACT' then 'Recommend to set to <EXACT>, <FORCE>
- Forcing SQL that does not use binding variables into binding variables'
-     when name = 'audit_trail' and value <> 'NONE' then 'If necessary set value to <NONE> to clos
-e Audit'
-     when name = 'sesssion_cached_cursors' and value > 100 then 'No need to set too large, Set th
-e maximum number of closed cursors that can be cached in each session.'
-     when name = 'deferred_segment_creation' and value <> 'FALSE' then '<TRUE> will result in the
- failure to export an empty table ,Mos 1216282.1'
-     when name = 'enable_ddl_logging' and value <> 'TRUE' then '<TRUE> enable alert log records D
-DL statements'
-     when name = 'undo_retention' and value < 1800 then 'Recommend to set to 10800 and set a larg
-e enough undo tablespace'
-     when name = 'statistics_level' and value <> 'TYPICAL' then 'Recommend to set it to the defau
-lt value <TYPICAL>'
-     when name = 'result_cache_mode' and value <> 'MANUAL' then 'Recommend to set to <MANUAL>'
-     when name = 'result_cache_max_size' and value <> 0 then 'Recommend to set to <0>'
-     when name = 'memory_target' and value <> 0 then 'Recommend to set to <0> to close AMM'
-     when name = 'memory_max_target' and value <> 0 then 'Recommend to set to <0> to close AMM'
-     when name = 'db_files' and value < 5000 then 'Recommend to set to more than <5000>'
-     when name = 'processes' and value < 5000 then 'Recommend to set to more than <5000>'
-     when name = 'job_queue_processes' and value < 1000 then 'Recommend to set to more than <1000
->'
-     when name = 'sga_target' and value = 0 then 'Recommend to set the same value with sga_max_si
-ze'
-     when name = 'sga_max_size' and value < 100 then 'According to the business and system memory
- settings'
-     when name = 'db_cache_size' and value = 0 then 'Recommend to set a minimum value'
-     when name = 'shared_pool_size' and value = 0 then 'Recommend to set a minimum value'
-     when name = 'pga_aggregate_target' and value < 100
-      then 'According to the business and system memory settings'
-     when name = 'fast_start_parallel_rollback' and value = 'FALSE'
-      then '<FALSE> means Parallel rollback is disabled, <LOW> Limits the maximum degree of paral
-lelism to 2 * CPU_COUNT'
-end as adivce
-from v$parameter
-where name in (
-'open_cursors',
-'control_file_record_keep_time',
-'cursor_sharing',
-'audit_trail',
-'session_cached_cursors',
-'deferred_segment_creation',
-'enable_ddl_logging',
-'undo_retention',
-'statistics_level',
-'result_cache_mode',
-'result_cache_max_size',
-'job_queue_processes',
-'processes',
-'db_files',
-'shared_pool_size',
-'db_cache_size',
-'pga_aggregate_target',
-'sga_target',
-'sga_max_size',
-'memory_target',
-'memory_max_target',
-'fast_start_parallel_rollback'
-);
+     when ksppinm = '_rollback_segment_count' and ksppstvl < '100'
+      then 'Recommend to set to <12000>'
+     when ksppinm = '_adg_parselock_timeout' and ksppstvl < '500'
+      then 'ADG Env set 500 To Avoid Instance Crash ref Doc ID 2183882.1 19C also default 0' end as advice
+      from x$ksppi x, x$ksppcv y
+      where x.indx = y.indx
+      and ksppinm in ('_kcfis_storageidx_diag_mode','__db_cache_size','_ksmg_granule_size',
+      '_gby_hash_aggregation_enabled','_external_scn_rejection_threshold_hours',
+      '_external_scn_logging_threshold_seconds',
+      '_ash_size',
+      '_lm_tickets',
+      '_lm_sync_timeout',
+      '_bloom_filter_enabled',
+      '_memory_imm_mode_without_autosga',
+      '_small_table_threshold',
+      '_very_large_object_threshold',
+      '_kcfis_storageidx_disabled',
+      '_serial_direct_read',
+      '_very_large_object_threshold',
+      '_adg_parselock_timeout',
+      '_max_outstanding_log_writes',
+      '_use_single_log_writer',
+      '_datafile_write_errors_crash_instance',
+      '_use_adaptive_log_file_sync',
+      '_undo_autotune',
+      '_optim_peek_user_binds',
+      '_optimizer_use_feedback',
+      '_optimizer_adaptive_cursor_sharing',
+      '_optimizer_extended_cursor_sharing',
+      '_optimizer_extended_cursor_sharing_rel',
+      'optimizer_index_caching',
+      'optimizer_index_cost_adj',
+      'db_file_multiblock_read_count',
+      'optimizer_dynamic_sampling',
+      '_cleanup_rollback_entries',
+      '_cursor_obsolete_threshold',
+      '_b_tree_bitmap_plans',
+      'optimizer_mode',
+      '_gc_policy_time',
+      '_gc_undo_affinity',
+      'optimizer_adaptive_plans',
+      '_PX_use_large_pool',
+      'parallel_force_local',
+      '_cursor_obsolete_threshold',
+      '_clusterwide_global_transactions',
+      'optimizer_adaptive_features',
+      '_optimizer_inmemory_access_path',
+      '_sql_plan_directive_mgmt_control',
+      '_optimizer_dsdir_usage_control',
+      '_rollback_segment_count',
+      '_highthreshold_undoretention'
+      )
+      union all
+      'db_check_db_parameter' as "db_check_db_parameter",
+      select
+      name,
+      value,
+      case when name = 'open_cursors' and (value < 300 or value > 1000) then 'open_cursors recommend to set to more than <300>, 3000 is enough'
+           when name = 'control_file_record_keep_time' and value <= 7 then 'control_file_record_keep_time recommend to set to <15>, 30 is enough,Doc 47322.1'
+           when name = 'cursor_sharing' and value <> 'EXACT' then 'Recommend to set to <EXACT>, <FORCE> Forcing SQL that does not use binding variables into binding variables'
+           when name = 'audit_trail' and value <> 'NONE' then 'If necessary set value to <NONE> to close Audit'
+           when name = 'sesssion_cached_cursors' and value > 100 then 'No need to set too large, Set the maximum number of closed cursors that can be cached in each session.'
+           when name = 'deferred_segment_creation' and value <> 'FALSE' then '<TRUE> will result in the failure to export an empty table ,Mos 1216282.1'
+           when name = 'enable_ddl_logging' and value <> 'TRUE' then '<TRUE> enable alert log records DDL statements'
+           when name = 'undo_retention' and value < 1800 then 'Recommend to set to 10800 and set a large enough undo tablespace'
+           when name = 'statistics_level' and value <> 'TYPICAL' then 'Recommend to set it to the default value <TYPICAL>'
+           when name = 'result_cache_mode' and value <> 'MANUAL' then 'Recommend to set to <MANUAL>'
+           when name = 'result_cache_max_size' and value <> 0 then 'Recommend to set to <0>'
+           when name = 'memory_target' and value <> 0 then 'Recommend to set to <0> to close AMM'
+           when name = 'memory_max_target' and value <> 0 then 'Recommend to set to <0> to close AMM'
+           when name = 'db_files' and value < 5000 then 'Recommend to set to more than <5000>'
+           when name = 'processes' and value < 5000 then 'Recommend to set to more than <5000>'
+           when name = 'job_queue_processes' and value < 1000 then 'Recommend to set to more than <1000>'
+           when name = 'sga_target' and value = 0 then 'Recommend to set the same value with sga_max_size'
+           when name = 'sga_max_size' and value < 100 then 'According to the business and system memory settings'
+           when name = 'db_cache_size' and value = 0 then 'Recommend to set a minimum value'
+           when name = 'shared_pool_size' and value = 0 then 'Recommend to set a minimum value eg set 20G'
+           when name = 'pga_aggregate_target' and value < 100 then 'According to the business and system memory settings'
+           when name = 'fast_start_parallel_rollback' and value = 'FALSE' then '<FALSE> means Parallel rollback is disabled, <LOW> Limits the maximum degree of parallelism to 2 * CPU_COUNT' end as adivce
+           from v$parameter
+           where name in (
+           'open_cursors',
+           'control_file_record_keep_time',
+           'cursor_sharing',
+           'audit_trail',
+           'session_cached_cursors',
+           'deferred_segment_creation',
+           'enable_ddl_logging',
+           'undo_retention',
+           'large_pool_size',
+           'statistics_level',
+           'result_cache_mode',
+           'result_cache_max_size',
+           'job_queue_processes',
+           'processes',
+           'db_files',
+           'shared_pool_size',
+           'db_cache_size',
+           'pga_aggregate_target',
+           'sga_target',
+           'sga_max_size',
+           'memory_target',
+           'memory_max_target',
+           'event',
+           'archive_lag_target',
+           'fast_start_parallel_rollback'
+           )
+           order by name;
