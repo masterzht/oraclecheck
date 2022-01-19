32d3c1e9892ea41b1019774aeddd931e5415f9cc
chongzi <15201061106@163.com>
2022-01-19 15:37:53 +0800

    modify

--- static/scripts/shell/del_listener.sh
+++ static/scripts/shell/del_listener.sh
@@ -25,30 +25,17 @@ else
 fi
 
 
-语句写法：find 对应目录 -mtime +天数 -name "文件名" -exec rm -rf {} \;
-
-　例1：　将/usr/local/backups目录下所有10天前带"."的文件删除
-
-　　find /usr/local/backups -mtime +10 -name "*.*" -exec rm -rf {} \;
-
-
-
-　　find：linux的查找命令，用户查找指定条件的文件
-
-　　/usr/local/backups：想要进行清理的任意目录
-
-　　-mtime：标准语句写法
-
-　　＋10：查找10天前的文件，这里用数字代表天数，＋30表示查找30天前的文件
-
-　　"*.*"：希望查找的数据类型，"*.jpg"表示查找扩展名为jpg的所有文件，"*"表示查找所有文件，这个可以灵活运用，举一反三
-
-　　-exec：固定写法
-
-　　rm -rf：强制删除文件，包括目录
-
-　　{} \; ：固定写法，一对大括号+空格+\
+#语句写法：find 对应目录 -mtime +天数 -name "文件名" -exec rm -rf {} \;
+#　例1：　将/usr/local/backups目录下所有10天前带"."的文件删除
+#　　find /usr/local/backups -mtime +10 -name "*.*" -exec rm -rf {} \;
+#　　find：linux的查找命令，用户查找指定条件的文件
+#　　/usr/local/backups：想要进行清理的任意目录
+#　　-mtime：标准语句写法
+#　　＋10：查找10天前的文件，这里用数字代表天数，＋30表示查找30天前的文件
+#　　"*.*"：希望查找的数据类型，"*.jpg"表示查找扩展名为jpg的所有文件，"*"表示查找所有文件，这个可以灵活运用，举一反三
+#　　-exec：固定写法
+#　　rm -rf：强制删除文件，包括目录
+#　　{} \; ：固定写法，一对大括号+空格+\
 
 
-
-find $1 -name "*.html" -mtime +1 -print0 |xargs -0 rm -v
+#  find $1 -name "*.html" -mtime +1 -print0 |xargs -0 rm -v
