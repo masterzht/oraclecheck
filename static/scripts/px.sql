d4a648e9699863bfa860808a9328f4ffbb161784
chongzi <15201061106@163.com>
2022-01-26 17:56:22 +0800

    modify

--- static/scripts/px.sql
+++ static/scripts/px.sql
@@ -7,46 +7,7 @@
 
 SET LINES 999 PAGES 50000 TRIMSPOOL ON TRIMOUT ON TAB OFF 
 
-COL px_qcsid HEAD QC_SID FOR A13
-COL px_instances FOR A100
 
-PROMPT Show current Parallel Execution sessions in RAC cluster...
-
-SELECT 
-    pxs.qcsid||','||pxs.qcserial# px_qcsid
-  , pxs.qcinst_id
-  , ses.username
-  , ses.sql_id
-  , pxs.degree
-  , pxs.req_degree
-  , COUNT(*) slaves
-  , COUNT(DISTINCT pxs.inst_id) inst_cnt
-  , MIN(pxs.inst_id) min_inst
-  , MAX(pxs.inst_id) max_inst 
-  --, LISTAGG ( TO_CHAR(pxs.inst_id) , ' ' ) WITHIN GROUP (ORDER BY pxs.inst_id) px_instances
-FROM 
-    gv$px_session pxs
-  , gv$session    ses
-  , gv$px_process p
-WHERE
-    ses.sid     = pxs.sid
-AND ses.serial# = pxs.serial#
-AND p.sid     = pxs.sid
-AND pxs.inst_id = ses.inst_id
-AND ses.inst_id = p.inst_id
---
-AND pxs.req_degree IS NOT NULL -- qc
-GROUP BY
-    pxs.qcsid||','||pxs.qcserial#
-  , pxs.qcinst_id
-  , ses.username
-  , ses.sql_id
-  , pxs.degree
-  , pxs.req_degree
-ORDER BY
-    pxs.qcinst_id
-  , slaves DESC
-/
 
 select a.sid, a.program, b.start_time, b.used_ublk,
        b.xidusn ||'.'|| b.xidslot || '.' || b.xidsqn trans_id
@@ -54,7 +15,8 @@ select a.sid, a.program, b.start_time, b.used_ublk,
  where a.taddr = b.addr
    and a.sid in ( select sid
                     from v$px_session
-                   where qcsid = &qcsid )
+                   --where qcsid = &qcsid
+                    )
  order by sid
 /
 prompt Display Parallel Execution QC and slave sessions for QC &1....
@@ -105,7 +67,7 @@ from
   , gv$session    s
   , gv$px_process p
 where 
-    pxs.qcsid in (&1)
+-- pxs.qcsid in (&1)
 --and s.sid     = pxs.qcsid
 and s.sid     = pxs.sid
 and s.serial# = pxs.serial#
