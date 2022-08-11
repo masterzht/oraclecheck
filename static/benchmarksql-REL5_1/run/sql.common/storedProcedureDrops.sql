182568dae4fb3b3deedcf4106812d94f3fafcabd
chongzi <15201061106@163.com>
2022-08-11 11:27:48 +0800

    modify

--- static/benchmarksql-REL5_1/run/sql.common/storedProcedureDrops.sql
+++
@@ -1,5 +0,0 @@
--- ----------------------------------------------------------------------
--- This is a placeholder for databases, that don't support
--- stored procedures or where the support is too limited to
--- reasonably implement the TPCC transaction profiles.
--- ----------------------------------------------------------------------
