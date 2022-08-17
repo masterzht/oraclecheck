d078505c8aaa720834136a83e289e9cd2784dfc7
chongzi <15201061106@163.com>
2022-08-17 15:41:48 +0800

    modify

--- static/scripts/shell/Pg/delarchive.sh
+++ static/scripts/shell/Pg/delarchive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . ~/.bashrc
 date +"%Y%m%d_%H%M%S"             >> /home/lightdb/yc/del_arch.log &&
-ltsql -U lightdb -p5555 -c "checkpoint;" >> /home/lightdb/yc/del_arch.log &&
+ltsql -U lightdb -p 5555 -c "checkpoint;" >> /home/lightdb/yc/del_arch.log &&
 echo "checkpoint complete"        >> /home/lightdb/yc/del_arch.log &&
 lt_archivecleanup $PGHOME/pg_wal `lt_controldata  | grep "Latest checkpoint's REDO WAL file" | awk '{print $6}'` >> /home/lightdb/yc/del_arch.log &&
 echo "archivelog delete complete" >> /home/lightdb/yc/del_arch.log
